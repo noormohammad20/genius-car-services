e828c1659ad4716127be13e70595888de4a7c8fe
noormohammad20 <mdsaim204@gmail.com>
2022-04-13 00:58:25 +0600

    Create Register form in vanilla css  and style it

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import { Button, Form } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
     const emailRef = useRef('')
@@ -39,7 +39,7 @@ const Login = () => {
                     Submit
                 </Button>
             </Form>
-            <p>New To Genius Car? <span onClick={navigateRegister} className='text-danger'>Please Register</span></p>
+            <p>New To Genius Car? <Link to='/register' onClick={navigateRegister} className='text-danger pe-auto text-decoration-none'>Please Register</Link></p>
         </div>
     )
 }
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import './Register.css'
 
 const Register = () => {
+    const navigate = useNavigate()
+
+    const navigateLogin = () => {
+        navigate('/login')
+    }
+    const handleRegister = e => {
+        e.preventDefault()
+    }
     return (
-        <div>
-            <h2>Register Please</h2>
-        </div>
+        <div className='register-form'>
+            <h2 style={{ textAlign: 'center' }}>Register Please</h2>
+            <form onSubmit={handleRegister} >
+                <input type="text" name="text" id="text" placeholder='Your Name' required />
+
+                <input type="email" name="Email" id="email" placeholder='Email Address' />
+
+                <input type="password" name="Password" id="password" placeholder='Password' required />
+
+                <input type="password" name="Confirm-password" id="confirm-password" placeholder='Confirm Password' required />
+
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account? <Link to='/login' onClick={navigateLogin} className='text-danger pe-auto text-decoration-none'>Please Login</Link></p>
+        </div >
     )
 }
 
