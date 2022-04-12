f3593b9538b48a4d3175df31f8b8c87d0defc167
noormohammad20 <mdsaim204@gmail.com>
2022-04-13 01:46:00 +0600

    Email password based authentication with react firebase hooks

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
 import './Register.css'
+import auth from '../../../firebase.init'
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth)
     const navigate = useNavigate()
 
     const navigateLogin = () => {
         navigate('/login')
+    }
+    if (user) {
+        navigate('/home')
     }
     const handleRegister = e => {
         e.preventDefault()
@@ -14,6 +25,8 @@ const Register = () => {
         const email = e.target.email.value
         const password = e.target.password.value
         const confirmPassword = e.target.confirmPassword.value
+
+        createUserWithEmailAndPassword(email, password)
     }
     return (
         <div className='register-form'>
