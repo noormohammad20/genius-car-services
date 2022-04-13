aa518cbdaac06792f53aa11aff5cb5b03adcd8ea
noormohammad20 <mdsaim204@gmail.com>
2022-04-13 21:52:38 +0600

    Style social login and share social login component

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import google from '../../../images/social/google.png'
+import facebook from '../../../images/social/facebook.png'
+import github from '../../../images/social/github.png'
 const SocialLogin = () => {
     return (
         <div>
@@ -7,6 +9,20 @@ const SocialLogin = () => {
                 <div style={{ height: '1px' }} className='bg-primary w-50'></div>
                 <p className='mt-2 px-2'>or</p>
                 <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+            </div>
+            <div>
+                <button className='btn btn-info w-50 d-block mx-auto my-4'>
+                    <img style={{ width: '30px' }} src={google} alt="" />
+                    <span className='px-2'> Google Sign IN</span>
+                </button>
+                <button className='btn btn-info w-50 d-block mx-auto my-4'>
+                    <img style={{ width: '30px' }} src={facebook} alt="" />
+                    <span className='px-2'> Facebook Sign IN</span>
+                </button>
+                <button className='btn btn-info w-50 d-block mx-auto my-4'>
+                    <img style={{ width: '30px' }} src={github} alt="" />
+                    <span className='px-2'> Github Sign IN</span>
+                </button>
             </div>
         </div>
     )
