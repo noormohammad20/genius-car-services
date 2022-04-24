9e6c90d5cceaa0beafbddd5b7eb095c57270728d
noormohammad20 <mdsaim204@gmail.com>
2022-04-24 20:30:57 +0600

    use costom hook forcheck out page

--- src/Pages/CheckOut/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut/CheckOut.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useServiceDetail from '../../../hooks/useServiceDetail'
 
 const CheckOut = () => {
+    const { serviceId } = useParams()
+    const [service] = useServiceDetail(serviceId)
     return (
-        <div>
-            <h2>Please CheckOut Your Booking</h2>
+        <div className='w-50 mx-auto'>
+            <h2>Please Service : {service.name}</h2>
+            <form>
+                <input type="text" name="name" placeholder='Name' />
+            </form>
         </div>
     )
 }
